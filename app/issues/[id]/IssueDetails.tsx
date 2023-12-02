31e47c06aa9efa0526c5a01ccba85b7209844444
Ashraful Islam <rx.ju.edu@gmail.com>
2023-12-02 23:31:25 +0700

    Refactor: Apply SRP

---
+++ app/issues/[id]/IssueDetails.tsx
@@ -0,0 +1,19 @@
+import { IssueStatusBadge } from "@/app/components";
+import { Issue } from "@prisma/client";
+import { Heading, Flex, Card, Text } from "@radix-ui/themes";
+import Markdown from "react-markdown";
+
+const IssueDetails = ({ issue }: { issue: Issue }) => (
+  <>
+    <Heading>{issue.title}</Heading>
+    <Flex my="2" className="space-x-3">
+      <Text>{issue.createdAt.toDateString()}</Text>
+      <IssueStatusBadge status={issue.status} />
+    </Flex>
+    <Card className="prose" mt="4">
+      <Markdown>{issue.description}</Markdown>
+    </Card>
+  </>
+);
+
+export default IssueDetails;
---
+++ app/issues/[id]/IssueEditButton.tsx
@@ -0,0 +1,12 @@
+import { Pencil2Icon } from "@radix-ui/react-icons";
+import { Button } from "@radix-ui/themes";
+import Link from "next/link";
+
+const IssueEditButton = ({ issueId }: { issueId: number }) => (
+  <Button>
+    <Pencil2Icon />
+    <Link href={`/issues/edit/${issueId}`}>Edit Issue</Link>
+  </Button>
+);
+
+export default IssueEditButton;
