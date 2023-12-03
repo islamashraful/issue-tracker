42ecf73717f24f990a38781f456c950c554b64bf
Ashraful Islam <rx.ju.edu@gmail.com>
2023-12-03 22:05:55 +0700

    Filter issues by status

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -1,13 +1,27 @@
 import prisma from "@/prisma/client";
 import { Table } from "@radix-ui/themes";
 import { IssueStatusBadge } from "../../components";
-import delay from "delay";
 import IssuesAction from "./IssuesAction";
 import { Link } from "../../components";
+import { Status } from "@prisma/client";
 
-const IssuesPage = async () => {
-  const issues = await prisma.issue.findMany();
-  await delay(1000);
+interface Props {
+  searchParams: {
+    status: Status;
+  };
+}
+
+const IssuesPage = async ({ searchParams }: Props) => {
+  const statuses = Object.values(Status);
+  const status = statuses.includes(searchParams.status)
+    ? searchParams.status
+    : undefined;
+
+  const issues = await prisma.issue.findMany({
+    where: {
+      status,
+    },
+  });
 
   return (
     <div>
