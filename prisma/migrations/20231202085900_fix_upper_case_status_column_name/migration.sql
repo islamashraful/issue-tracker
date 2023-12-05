b68f1d91649810d9978f21896aa117832199b05f
Ashraful Islam <rx.ju.edu@gmail.com>
2023-12-05 12:00:56 +0700

    Set relationMode to prisma

--- prisma/migrations/20231202085900_fix_upper_case_status_column_name/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `Status` on the `Issue` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `Issue` DROP COLUMN `Status`,
-    ADD COLUMN `status` ENUM('OPEN', 'IN_PROGRESS', 'CLOSED') NOT NULL DEFAULT 'OPEN';
